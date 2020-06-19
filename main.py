0b93054de54ddf369969400828eb85a0e4860e6e
Ang3l1t0 <1546@holbertonschool.com>
2020-06-18 23:56:40 -0500

    add right and left movement

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+"""MyGame
+"""
 import os.path
 import pygame
 
-current_path = os.path.dirname(__file__) # Where your .py file is located
-resource_path = os.path.join(current_path, '/home/angel/Documentos/Holberton/MyGameProject/') # The resource folder path
-image_path = os.path.join(resource_path, 'img') # The image folder path
+current_path = os.path.dirname(__file__)
+resource_path = os.path.join(current_path,
+                             '/home/angel/Documentos/Holberton/MyGameProject/')
+image_path = os.path.join(resource_path, 'img')
 
 
 # Initialize pygame
@@ -18,24 +21,37 @@ icon = pygame.image.load(os.path.join(image_path, 'alien.png'))
 pygame.display.set_icon(icon)
 
 # Player
-playerImg = pygame.image.load(os.path.join(image_path, 'alienbig.png'))
-playerX = 150
-playerY = 50
+playerImg = pygame.image.load(os.path.join(image_path, 'spaceship.png'))
+PLAYER_X = 350
+PLAYER_Y = 500
+PLAYER_X_CHANGE = 0
 
 
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    """player function
+    """
+    screen.blit(playerImg, (x, y))
 
 
 # Game Loop
-running = True
-while running:
+RUNNING = True
+while RUNNING:
     # RGB - RED, GREEN, BLUE
     screen.fill((0, 0, 128))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            RUNNING = False
 
-    player()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                PLAYER_X_CHANGE -= 0.5
+            if event.key == pygame.K_RIGHT:
+                PLAYER_X_CHANGE += 0.5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                PLAYER_X_CHANGE = 0
+
+    PLAYER_X += PLAYER_X_CHANGE
+    player(PLAYER_X, PLAYER_Y)
     pygame.display.update()
