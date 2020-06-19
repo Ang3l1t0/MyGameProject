35946647dd1ab8e94eda6f866507b56d2021d339
Ang3l1t0 <1546@holbertonschool.com>
2020-06-18 22:27:22 -0500

    create img directory

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+import os.path
 import pygame
+
+current_path = os.path.dirname(__file__) # Where your .py file is located
+resource_path = os.path.join(current_path, '/home/angel/Documentos/Holberton/MyGameProject/') # The resource folder path
+image_path = os.path.join(resource_path, 'img') # The image folder path
+
 
 # Initialize pygame
 pygame.init()
@@ -7,17 +13,19 @@ pygame.init()
 screen = pygame.display.set_mode((800, 600))
 
 # Title and Icon
-pygame.display.set_caption("Allien Killer")
-icon = pygame.image.load("alien.png")
+pygame.display.set_caption('Allien Killer')
+icon = pygame.image.load(os.path.join(image_path, 'alien.png'))
 pygame.display.set_icon(icon)
 
 # Player
-playerImg = pygame.image.load("alienbig.png")
+playerImg = pygame.image.load(os.path.join(image_path, 'alienbig.png'))
 playerX = 150
 playerY = 50
 
+
 def player():
     screen.blit(playerImg, (playerX, playerY))
+
 
 # Game Loop
 running = True
